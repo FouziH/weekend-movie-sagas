68ae3e625d28e7b432e3ed5eb5c614802f76b10b
Hamza Mohamed <Hamzafowzi1@gmail.com>
2021-08-30 07:24:17 -0500

    Added comments tto my details.js file

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,6 +1,7 @@
 import {  useSelector } from "react-redux";
 import DetailsItem from "../DetailsItem/DetailsItems";
 function Details() {
+  //calling my store reducer responsible for getting movie details and all genres 
     const movieDetailsAndGenres = useSelector(
       (store) => store.getMovieDetailsAndGenres
     );
@@ -12,7 +13,9 @@ function Details() {
       <br />
       <br />
       <div>
+        {/* mapping through the store reducer variable  */}
         {movieDetailsAndGenres.map((items, i) => (
+          // calling my details page and passing item as prop
           <DetailsItem key={i} items={items} />
         ))}
       </div>
